ede0fa07b07a600a6dbf57a5ba4c3366878e8abe
Alexander Dm. <dabl21291@gmail.com>
2024-12-07 01:17:20 +0300

    Добавьте файлы проекта.

---
+++ OOP/OOP.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+class Book {
+    char* Author;
+    char* Text;
+    int pages;
+    int year;
+
+public:
+    Book() = default;
+
+    Book(const Book&) = delete;
+    Book& operator=(const Book&) = delete;
+
+    Book(Book&& obj) noexcept
+        : Author(obj.Author), Text(obj.Text), pages(obj.pages), year(obj.year) {
+        obj.Author = nullptr;
+        obj.Text = nullptr;
+    }
+
+    Book& operator=(Book&& obj) noexcept {
+        if (this != &obj) {
+            delete[] Author;
+            delete[] Text;
+
+            Author = obj.Author;
+            Text = obj.Text;
+            pages = obj.pages;
+            year = obj.year;
+
+            obj.Author = nullptr;
+            obj.Text = nullptr;
+        }
+        return *this;
+    }
+
+    ~Book() {
+        delete[] Author;
+        delete[] Text;
+    }
+};
+
+int main()
+{
+
+
+
+
+};
